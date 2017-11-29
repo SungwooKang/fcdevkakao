6fd140313fcbb9a2abce0acf3a000d810bf9db4a
SungwooKang <swkang@fastcampus.co.kr>
2017-11-29 13:50:38 +0900

    test2

--- kakao/views.py
+++ kakao/views.py
@@ -5,6 +5,6 @@ from django.http import JsonResponse
 def keyboard(request):
 
     return JsonResponse({
-        'type' : 'buttons'
-        'buttons' : ['파트타임 교육과정', '풀타임 교육과정', '기타']
+        'type' : 'buttons',
+        'buttons' : ['1','2']
     })
