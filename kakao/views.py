35307d187d3012aed38293b63d4e25a0d3c60502
SungwooKang <swkang@fastcampus.co.kr>
2017-11-29 10:29:06 +0900

    test

---
+++ kakao/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.http import JsonResponse
+# # Create your views here.
+
+# def keyboard(request):
+
+#     return JsonResponse({
+#         'type' : 'buttons'
+#         'buttons' : ['파트타임 교육과정', '풀타임 교육과정', '기타']
+#     })
