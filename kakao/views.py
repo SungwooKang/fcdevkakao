c969edbbe4232e09dd240bbfad4d707e291e4b9a
SungwooKang <swkang@fastcampus.co.kr>
2017-12-02 16:40:50 +0900

    test

--- kakao/views.py
+++ kakao/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-# import models
+import datetime
+import requests
+from bs4 import BeautifulSoup as bs
 # Create your views here.
 
 def keyboard(request):
@@ -20,7 +22,13 @@ def message(request):
         message = ((request.body).decode('utf-8'))
         return_json_str = json.loads(message)
         return_str = return_json_str['content']
-        # kkk = models.lotto()
+        response = requests.get('http://www.nlotto.co.kr/gameResult.do?method=byWin')
+        result = bs(response.text, 'html.parser')
+        numbers = [tag['alt'] for tag in result.select('.contentsArticle p.number img')]
+        count = result.find("h3",{"class":"result_title"}).text.split('\n')
+        c1 = count[2]
+        c2 = c1[1:-1]
+        c3 = "제 {0} 회차 로또 당첨 번호는 {1}, {2}, {3}, {4}, {5}, {6} 이며, 2등 당첨 번호는 {7} 입니다.".format(count[0], numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6])
 
         if  return_str == '실무 맞춤형 교육 (저녁/주말)':
             return JsonResponse({
@@ -100,14 +108,14 @@ def message(request):
                 }     
             
             })
-        # elif return_str == '로또':
-        #     return JsonResponse({
+        elif return_str == '로또':
+            return JsonResponse({
 
-        #         'message': {
-        #             'text': kkk
-        #         }
+                'message': {
+                    'text': c2 + '\n' +c3
+                }
 
-        #     })
+            })
         else:
          
             return JsonResponse({
