9ebf8bbc72f88d09575988e8c25cbe16b89c381a
SungwooKang <swkang@fastcampus.co.kr>
2017-12-02 16:34:27 +0900

    test

--- kakao/models.py
+++ kakao/models.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*- 
 
 from django.db import models
-import datetime
-import requests
-from bs4 import BeautifulSoup as bs
+# import datetime
+# import requests
+# from bs4 import BeautifulSoup as bs
 
-# Create your models here.
-def lotto():
-    # now = datetime.datetime.now()
-    # nh = datetime.time()
-    # lh = datetime.time(20,50)
+# def lotto():
 
-    # day = datetime.datetime.today().weekday()
 
-    response = requests.get('http://www.nlotto.co.kr/gameResult.do?method=byWin')
-    result = bs(response.text, 'html.parser')
-    numbers = [tag['alt'] for tag in result.select('.contentsArticle p.number img')]
-    count = result.find("h3",{"class":"result_title"}).text.split('\n')
-    c1 = count[2]
-    c2 = c1[1:-1]
-    c3 = "제 {0} 회차 로또 당첨 번호는 {1}, {2}, {3}, {4}, {5}, {6} 이며, 2등 당첨 번호는 {7} 입니다.".format(count[0], numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6])
-    return c2, c3
+#     response = requests.get('http://www.nlotto.co.kr/gameResult.do?method=byWin')
+#     result = bs(response.text, 'html.parser')
+#     numbers = [tag['alt'] for tag in result.select('.contentsArticle p.number img')]
+#     count = result.find("h3",{"class":"result_title"}).text.split('\n')
+#     c1 = count[2]
+#     c2 = c1[1:-1]
+#     c3 = "제 {0} 회차 로또 당첨 번호는 {1}, {2}, {3}, {4}, {5}, {6} 이며, 2등 당첨 번호는 {7} 입니다.".format(count[0], numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6])
+#     return c2, c3
--- kakao/views.py
+++ kakao/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-import models
+# import models
 # Create your views here.
 
 def keyboard(request):
